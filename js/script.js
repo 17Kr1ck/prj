ed4a4012ed1eedb9510283eb4a48235130e2a99e
17Kr1ck <cryptavlad@gmail.com>
2024-02-19 23:22:41 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+
+const obj = {
+  a: 50
+};
+
+obj.a = 10;
+
+console.log(obj.a);
+
+{
+  // let result = 50;
+}
+
+console.log(result);
+
+const user = 'John';
+console.log(user);
+let number = 25;
+number = 24;
+
+storeName = 'смішні ціни';
+storeDescription = {
+  budget: 10000,
+  employees: []
+};
